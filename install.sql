87e06785666e58b4a4a8d052bf47f6a67c47e813
Tyler Ipson <tipson@1on1.com>
2013-07-31 09:32:03 -0600

    Cleaning up and removing Ensemble stuff

--- install.sql
+++ install.sql
@@ -1,92 +1,4 @@
 
--- *** STRUCTURE: `tbl_authors` ***
-DROP TABLE IF EXISTS `tbl_authors`;
-CREATE TABLE `tbl_authors` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `username` varchar(20) NOT NULL DEFAULT '',
-  `password` varchar(40) NOT NULL,
-  `first_name` varchar(100) DEFAULT NULL,
-  `last_name` varchar(100) DEFAULT NULL,
-  `email` varchar(255) DEFAULT NULL,
-  `last_seen` datetime DEFAULT '0000-00-00 00:00:00',
-  `user_type` enum('author','developer') NOT NULL DEFAULT 'author',
-  `primary` enum('yes','no') NOT NULL DEFAULT 'no',
-  `default_area` varchar(255) DEFAULT NULL,
-  `auth_token_active` enum('yes','no') NOT NULL DEFAULT 'no',
-  `language` varchar(15) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `username` (`username`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1;
-
--- *** STRUCTURE: `tbl_cache` ***
-DROP TABLE IF EXISTS `tbl_cache`;
-CREATE TABLE `tbl_cache` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `hash` varchar(32) NOT NULL DEFAULT '',
-  `creation` int(14) NOT NULL DEFAULT '0',
-  `expiry` int(14) unsigned DEFAULT NULL,
-  `data` longtext NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `hash` (`hash`),
-  KEY `expiry` (`expiry`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-
--- *** STRUCTURE: `tbl_entries` ***
-DROP TABLE IF EXISTS `tbl_entries`;
-CREATE TABLE `tbl_entries` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `section_id` int(11) unsigned NOT NULL,
-  `author_id` int(11) unsigned NOT NULL,
-  `creation_date` datetime NOT NULL,
-  `creation_date_gmt` datetime NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `section_id` (`section_id`),
-  KEY `author_id` (`author_id`),
-  KEY `creation_date` (`creation_date`),
-  KEY `creation_date_gmt` (`creation_date_gmt`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-
--- *** STRUCTURE: `tbl_extensions` ***
-DROP TABLE IF EXISTS `tbl_extensions`;
-CREATE TABLE `tbl_extensions` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) NOT NULL DEFAULT '',
-  `status` enum('enabled','disabled') NOT NULL DEFAULT 'enabled',
-  `version` varchar(20) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `name` (`name`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1;
-
--- *** STRUCTURE: `tbl_extensions_delegates` ***
-DROP TABLE IF EXISTS `tbl_extensions_delegates`;
-CREATE TABLE `tbl_extensions_delegates` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `extension_id` int(11) NOT NULL,
-  `page` varchar(100) NOT NULL,
-  `delegate` varchar(100) NOT NULL,
-  `callback` varchar(100) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `extension_id` (`extension_id`),
-  KEY `page` (`page`),
-  KEY `delegate` (`delegate`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1;
-
--- *** STRUCTURE: `tbl_fields` ***
-DROP TABLE IF EXISTS `tbl_fields`;
-CREATE TABLE `tbl_fields` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `label` varchar(255) NOT NULL,
-  `element_name` varchar(50) NOT NULL,
-  `type` varchar(32) NOT NULL,
-  `parent_section` int(11) NOT NULL DEFAULT '0',
-  `required` enum('yes','no') NOT NULL DEFAULT 'yes',
-  `sortorder` int(11) NOT NULL DEFAULT '1',
-  `location` enum('main','sidebar') NOT NULL DEFAULT 'main',
-  `show_column` enum('yes','no') NOT NULL DEFAULT 'no',
-  PRIMARY KEY (`id`),
-  KEY `index` (`element_name`,`type`,`parent_section`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-
 -- *** STRUCTURE: `tbl_fields_author` ***
 DROP TABLE IF EXISTS `tbl_fields_author`;
 CREATE TABLE `tbl_fields_author` (
@@ -99,6 +11,8 @@ CREATE TABLE `tbl_fields_author` (
   UNIQUE KEY `field_id` (`field_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
+-- *** DATA: `tbl_fields_author` ***
+
 -- *** STRUCTURE: `tbl_fields_checkbox` ***
 DROP TABLE IF EXISTS `tbl_fields_checkbox`;
 CREATE TABLE `tbl_fields_checkbox` (
@@ -110,6 +24,8 @@ CREATE TABLE `tbl_fields_checkbox` (
   KEY `field_id` (`field_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
+-- *** DATA: `tbl_fields_checkbox` ***
+
 -- *** STRUCTURE: `tbl_fields_date` ***
 DROP TABLE IF EXISTS `tbl_fields_date`;
 CREATE TABLE `tbl_fields_date` (
@@ -120,6 +36,8 @@ CREATE TABLE `tbl_fields_date` (
   KEY `field_id` (`field_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
+-- *** DATA: `tbl_fields_date` ***
+
 -- *** STRUCTURE: `tbl_fields_input` ***
 DROP TABLE IF EXISTS `tbl_fields_input`;
 CREATE TABLE `tbl_fields_input` (
@@ -129,6 +47,8 @@ CREATE TABLE `tbl_fields_input` (
   PRIMARY KEY (`id`),
   KEY `field_id` (`field_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- *** DATA: `tbl_fields_input` ***
 
 -- *** STRUCTURE: `tbl_fields_select` ***
 DROP TABLE IF EXISTS `tbl_fields_select`;
@@ -144,6 +64,8 @@ CREATE TABLE `tbl_fields_select` (
   KEY `field_id` (`field_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
+-- *** DATA: `tbl_fields_select` ***
+
 -- *** STRUCTURE: `tbl_fields_selectbox_link` ***
 DROP TABLE IF EXISTS `tbl_fields_selectbox_link`;
 CREATE TABLE `tbl_fields_selectbox_link` (
@@ -157,6 +79,8 @@ CREATE TABLE `tbl_fields_selectbox_link` (
   KEY `field_id` (`field_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
+-- *** DATA: `tbl_fields_selectbox_link` ***
+
 -- *** STRUCTURE: `tbl_fields_taglist` ***
 DROP TABLE IF EXISTS `tbl_fields_taglist`;
 CREATE TABLE `tbl_fields_taglist` (
@@ -169,6 +93,8 @@ CREATE TABLE `tbl_fields_taglist` (
   KEY `pre_populate_source` (`pre_populate_source`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
+-- *** DATA: `tbl_fields_taglist` ***
+
 -- *** STRUCTURE: `tbl_fields_textarea` ***
 DROP TABLE IF EXISTS `tbl_fields_textarea`;
 CREATE TABLE `tbl_fields_textarea` (
@@ -179,6 +105,8 @@ CREATE TABLE `tbl_fields_textarea` (
   PRIMARY KEY (`id`),
   KEY `field_id` (`field_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- *** DATA: `tbl_fields_textarea` ***
 
 -- *** STRUCTURE: `tbl_fields_upload` ***
 DROP TABLE IF EXISTS `tbl_fields_upload`;
@@ -191,75 +119,50 @@ CREATE TABLE `tbl_fields_upload` (
   KEY `field_id` (`field_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
--- *** STRUCTURE: `tbl_forgotpass` ***
-DROP TABLE IF EXISTS `tbl_forgotpass`;
-CREATE TABLE `tbl_forgotpass` (
-  `author_id` int(11) NOT NULL DEFAULT '0',
-  `token` varchar(6) NOT NULL,
-  `expiry` varchar(25) NOT NULL,
-  PRIMARY KEY (`author_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+-- *** DATA: `tbl_fields_upload` ***
 
--- *** STRUCTURE: `tbl_pages` ***
-DROP TABLE IF EXISTS `tbl_pages`;
-CREATE TABLE `tbl_pages` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `parent` int(11) DEFAULT NULL,
-  `title` varchar(255) NOT NULL DEFAULT '',
-  `handle` varchar(255) DEFAULT NULL,
-  `path` varchar(255) DEFAULT NULL,
-  `params` varchar(255) DEFAULT NULL,
-  `data_sources` text,
-  `events` text,
-  `sortorder` int(11) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`id`),
-  KEY `parent` (`parent`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1;
+-- *** DATA: `tbl_entries` ***
 
--- *** STRUCTURE: `tbl_pages_types` ***
-DROP TABLE IF EXISTS `tbl_pages_types`;
-CREATE TABLE `tbl_pages_types` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `page_id` int(11) unsigned NOT NULL,
-  `type` varchar(50) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `page_id` (`page_id`,`type`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1;
+-- *** DATA: `tbl_extensions` ***
+INSERT INTO `tbl_extensions` (`id`, `name`, `status`, `version`) VALUES (1, 'xssfilter', 'enabled', 1.1);
+INSERT INTO `tbl_extensions` (`id`, `name`, `status`, `version`) VALUES (2, 'debugdevkit', 'enabled', '1.2.1');
+INSERT INTO `tbl_extensions` (`id`, `name`, `status`, `version`) VALUES (3, 'export_ensemble', 'enabled', 1.16);
+INSERT INTO `tbl_extensions` (`id`, `name`, `status`, `version`) VALUES (4, 'selectbox_link_field', 'enabled', 1.22);
+INSERT INTO `tbl_extensions` (`id`, `name`, `status`, `version`) VALUES (5, 'jit_image_manipulation', 'enabled', 1.14);
+INSERT INTO `tbl_extensions` (`id`, `name`, `status`, `version`) VALUES (6, 'maintenance_mode', 'enabled', 1.5);
+INSERT INTO `tbl_extensions` (`id`, `name`, `status`, `version`) VALUES (7, 'profiledevkit', 'enabled', '1.0.4');
+INSERT INTO `tbl_extensions` (`id`, `name`, `status`, `version`) VALUES (8, 'markdown', 'enabled', 1.13);
+INSERT INTO `tbl_extensions` (`id`, `name`, `status`, `version`) VALUES (10, 'html5_doctype', 'enabled', '1.2.4');
 
--- *** STRUCTURE: `tbl_sections` ***
-DROP TABLE IF EXISTS `tbl_sections`;
-CREATE TABLE `tbl_sections` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) NOT NULL DEFAULT '',
-  `handle` varchar(255) NOT NULL,
-  `sortorder` int(11) NOT NULL DEFAULT '0',
-  `entry_order` varchar(7) DEFAULT NULL,
-  `entry_order_direction` enum('asc','desc') DEFAULT 'asc',
-  `hidden` enum('yes','no') NOT NULL DEFAULT 'no',
-  `navigation_group` varchar(255) NOT NULL DEFAULT 'Content',
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `handle` (`handle`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+-- *** DATA: `tbl_extensions_delegates` ***
+INSERT INTO `tbl_extensions_delegates` (`id`, `extension_id`, `page`, `delegate`, `callback`) VALUES (27, 1, '/blueprints/events/edit/', 'AppendEventFilter', 'appendEventFilter');
+INSERT INTO `tbl_extensions_delegates` (`id`, `extension_id`, `page`, `delegate`, `callback`) VALUES (26, 1, '/blueprints/events/new/', 'AppendEventFilter', 'appendEventFilter');
+INSERT INTO `tbl_extensions_delegates` (`id`, `extension_id`, `page`, `delegate`, `callback`) VALUES (35, 2, '/frontend/', 'ManipulateDevKitNavigation', 'manipulateDevKitNavigation');
+INSERT INTO `tbl_extensions_delegates` (`id`, `extension_id`, `page`, `delegate`, `callback`) VALUES (34, 2, '/frontend/', 'FrontendDevKitResolve', 'frontendDevKitResolve');
+INSERT INTO `tbl_extensions_delegates` (`id`, `extension_id`, `page`, `delegate`, `callback`) VALUES (31, 3, '/system/preferences/', 'AddCustomPreferenceFieldsets', 'appendPreferences');
+INSERT INTO `tbl_extensions_delegates` (`id`, `extension_id`, `page`, `delegate`, `callback`) VALUES (46, 5, '/system/preferences/', 'Save', '__SavePreferences');
+INSERT INTO `tbl_extensions_delegates` (`id`, `extension_id`, `page`, `delegate`, `callback`) VALUES (42, 6, '/blueprints/pages/', 'AppendPageContent', '__appendType');
+INSERT INTO `tbl_extensions_delegates` (`id`, `extension_id`, `page`, `delegate`, `callback`) VALUES (41, 6, '/backend/', 'AppendPageAlert', '__appendAlert');
+INSERT INTO `tbl_extensions_delegates` (`id`, `extension_id`, `page`, `delegate`, `callback`) VALUES (40, 6, '/system/preferences/', 'CustomActions', '__toggleMaintenanceMode');
+INSERT INTO `tbl_extensions_delegates` (`id`, `extension_id`, `page`, `delegate`, `callback`) VALUES (39, 6, '/system/preferences/', 'Save', '__SavePreferences');
+INSERT INTO `tbl_extensions_delegates` (`id`, `extension_id`, `page`, `delegate`, `callback`) VALUES (38, 6, '/system/preferences/', 'AddCustomPreferenceFieldsets', 'appendPreferences');
+INSERT INTO `tbl_extensions_delegates` (`id`, `extension_id`, `page`, `delegate`, `callback`) VALUES (16, 7, '/frontend/', 'FrontendDevKitResolve', 'frontendDevKitResolve');
+INSERT INTO `tbl_extensions_delegates` (`id`, `extension_id`, `page`, `delegate`, `callback`) VALUES (17, 7, '/frontend/', 'ManipulateDevKitNavigation', 'manipulateDevKitNavigation');
+INSERT INTO `tbl_extensions_delegates` (`id`, `extension_id`, `page`, `delegate`, `callback`) VALUES (22, 10, '/frontend/', 'FrontendOutputPostGenerate', 'parse_html');
+INSERT INTO `tbl_extensions_delegates` (`id`, `extension_id`, `page`, `delegate`, `callback`) VALUES (45, 5, '/system/preferences/', 'AddCustomPreferenceFieldsets', 'appendPreferences');
+INSERT INTO `tbl_extensions_delegates` (`id`, `extension_id`, `page`, `delegate`, `callback`) VALUES (28, 1, '/frontend/', 'EventPreSaveFilter', 'eventPreSaveFilter');
+INSERT INTO `tbl_extensions_delegates` (`id`, `extension_id`, `page`, `delegate`, `callback`) VALUES (43, 6, '/frontend/', 'FrontendPrePageResolve', '__checkForMaintenanceMode');
+INSERT INTO `tbl_extensions_delegates` (`id`, `extension_id`, `page`, `delegate`, `callback`) VALUES (44, 6, '/frontend/', 'FrontendParamsResolve', '__addParam');
 
--- *** STRUCTURE: `tbl_sections_association` ***
-DROP TABLE IF EXISTS `tbl_sections_association`;
-CREATE TABLE `tbl_sections_association` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `parent_section_id` int(11) unsigned NOT NULL,
-  `parent_section_field_id` int(11) unsigned DEFAULT NULL,
-  `child_section_id` int(11) unsigned NOT NULL,
-  `child_section_field_id` int(11) unsigned NOT NULL,
-  `hide_association` enum('yes','no') NOT NULL DEFAULT 'no',
-  PRIMARY KEY (`id`),
-  KEY `parent_section_id` (`parent_section_id`,`child_section_id`,`child_section_field_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+-- *** DATA: `tbl_fields` ***
 
--- *** STRUCTURE: `tbl_sessions` ***
-DROP TABLE IF EXISTS `tbl_sessions`;
-CREATE TABLE `tbl_sessions` (
-  `session` varchar(255) NOT NULL,
-  `session_expires` int(10) unsigned NOT NULL DEFAULT '0',
-  `session_data` text,
-  PRIMARY KEY (`session`),
-  KEY `session_expires` (`session_expires`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+-- *** DATA: `tbl_pages` ***
+INSERT INTO `tbl_pages` (`id`, `parent`, `title`, `handle`, `path`, `params`, `data_sources`, `events`, `sortorder`) VALUES (1, NULL, 'Home', 'home', NULL, NULL, 'navigation', NULL, 4);
+INSERT INTO `tbl_pages` (`id`, `parent`, `title`, `handle`, `path`, `params`, `data_sources`, `events`, `sortorder`) VALUES (2, NULL, 'Grid Example', 'grid-example', NULL, NULL, 'navigation', NULL, 5);
+
+-- *** DATA: `tbl_pages_types` ***
+INSERT INTO `tbl_pages_types` (`id`, `page_id`, `type`) VALUES (2, 1, 'index');
+
+-- *** DATA: `tbl_sections` ***
+
+-- *** DATA: `tbl_sections_association` ***
--- workspace/assets/js/site.js
+++
@@ -1,6 +0,0 @@
-head.ready(function(){
-	// Padding on grid elements for IE7, since it doesn't support box-sizing
-	$('.lt-ie8 .row .pad').each(function(){
-		$(this).wrapInner('<div class="padding"/>');
-	});
-});
